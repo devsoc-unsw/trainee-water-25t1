6dffd26ca4d10de0b4b0a374fc35b313cc8f8b42
Dylan Hu <hudylan787@gmail.com>
2025-05-17 17:44:05 +1000

    add habit page

---
+++ accountabet/src/app/addhabit/page.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@/components/ui/input";
+
+export default function AddHabit() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1>Add A Habit:</h1>
+      <Input />
+    </div>
+  );
+}
