09fb81a9fa85079b4c14a1fe64d874b7a618c749
Vedant Chanpura <vedantchanpura@gmail.com>
2025-05-17 18:32:32 +1000

    database setup half

---
+++ accountabet/src/app/api/test/route.ts
@@ -0,0 +1,10 @@
+// // import { db } from "@/lib/db";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   const user = await db.user.findUnique({
+//     where: { email: "leo@example.com" },
+//   });
+
+//   return NextResponse.json(user);
+// }
